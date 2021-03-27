1bd282fbd4db492c1ad24748823e037338298650
ANDREW-NB\numva <numv.andrew@gmail.com>
2021-03-27 10:39:05 +0300

    Готов начальный проект, работает кнопка "Построить" Но замечены некоторые странности: 1) При наведении мышкой на любую из 3-х кнопок, график меняет цвет на новырандомный Больше пока что замечаний нет, ошибок тоде не замечено

--- laba4-rabota_s_phailami/Form1.Designer.cs
+++ laba4-rabota_s_phailami/Form1.Designer.cs
@@ -24,16 +24,65 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (254)));
+            this.button1.Location = new System.Drawing.Point(130, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ПОСТРОИТЬ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (254)));
+            this.button2.Location = new System.Drawing.Point(296, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "СЧИТАТЬ";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (254)));
+            this.button3.Location = new System.Drawing.Point(420, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 32);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "ЗАПИСАТЬ";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1084, 511);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+
+        private System.Windows.Forms.Button button1;
 
         #endregion
     }
--- laba4-rabota_s_phailami/Form1.cs
+++ laba4-rabota_s_phailami/Form1.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,61 @@ namespace laba4_rabota_s_phailami
 {
     public partial class Form1 : Form
     {
+        List<int> Mas = new List<int>();
+        int maxy, count;
+       
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            Random r = new Random();
+            count = r.Next(5, 20);
+            Mas.Clear();
+            for (int i = 0; i < count; i++)  Mas.Add(r.Next(-1000, 1000));
+            
+            maxy = (Mas.Max());
+            if (maxy < (int)Math.Abs(Mas.Min())) maxy = (int)Math.Abs(Mas.Min());
+
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            //рисуем поле 
+            int xc = 100;
+            int yc = (this.Height) / 2;
+            g.TranslateTransform(xc, yc);
+            g.DrawLine(new Pen(Color.Black, 2f), 0, -250, 0, 250);
+            g.DrawLine(new Pen(Color.Black, 2f), 0, 0, 1000, 0);
+            
+            float step = 1000f / (float)count;
+            float k = Math.Abs(250f / (float)maxy);
+            
+            Random rn = new Random();
+            SolidBrush br = new SolidBrush(Color.Black);
+            Font stringFont = new Font("Times New Roman", 10, FontStyle.Regular);
+            SizeF ss = new SizeF();
+
+            for (var i = 0; i < count; i++)
+            {
+                float width = step / 2;
+                br.Color = Color.FromArgb(rn.Next(255), rn.Next(255), rn.Next(255));
+                ss = e.Graphics.MeasureString(Mas[i].ToString(), stringFont);
+                if (Mas[i] < 0)
+                {
+                    g.FillRectangle(br,(i + 0.5f)*step,1f,width,-1f*Mas[i]*k);
+                    g.DrawString(Mas[i].ToString(), stringFont, new SolidBrush(Color.Black), new PointF((i + 0.75f)*step-ss.Width/2, -(Mas[i]*k)));
+                }
+                if (Mas[i] >= 0)
+                {
+                    g.FillRectangle(br,(i + 0.5f)*step,((-1)*Mas[i]*k)-1,width,Mas[i]*k);
+                    g.DrawString(Mas[i].ToString(), stringFont, new SolidBrush(Color.Black), new PointF((i + 0.75f)*step-ss.Width/2, -(Mas[i]*k+15)));
+                }
+            }
         }
     }
 }
